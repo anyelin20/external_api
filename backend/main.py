ed3a1a106fae146dbbe642b9bde1a28e2e1c2419
Deykel17 <161171059+Deykel17@users.noreply.github.com>
2025-07-17 23:07:21 -0600

    Agrega rutas para manejar formulario de ingreso de datos

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from weather import router as weather_router
+from ingreso import router as ingreso_router  # <- NUEVO IMPORTANTE
 
 app = FastAPI(
     title="API del Clima",
@@ -20,5 +21,6 @@ app.add_middleware(
 
 # Rutas
 app.include_router(weather_router.router)
+app.include_router(ingreso_router.router)  # <- NUEVO PARA EL FORMULARIO
 
 # Uvicorn para ejecutar: uvicorn backend.main:app --reload
