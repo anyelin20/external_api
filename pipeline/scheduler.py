77702dc00b57dc01e8fc7100f0397d70e1348096
Deykel17 <161171059+Deykel17@users.noreply.github.com>
2025-08-01 19:57:11 -0600

    Update scheduler.py

--- pipeline/scheduler.py
+++ pipeline/scheduler.py
@@ -2,7 +2,11 @@ import schedule
 import time
 from pipeline.etl_pipeline import ejecutar_pipeline
 
-schedule.every(10).minutes.do(ejecutar_pipeline)
+def job():
+    resultado = ejecutar_pipeline()
+    print(f"Pipeline ejecutado: {resultado}")
+
+schedule.every(10).minutes.do(job)
 
 while True:
     schedule.run_pending()
